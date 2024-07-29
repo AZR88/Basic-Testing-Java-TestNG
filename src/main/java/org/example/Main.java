55f45b5e7b4584e169a43b1e886fcad5ad1b579f
AZR88 <azrielamaldany@gmail.com>
2024-07-29 22:48:08 +0700

    update function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public class penjumlahan {
+    public static class  penjumlahan {
         public int add(int a, int b) {
             return a + b;
         }
