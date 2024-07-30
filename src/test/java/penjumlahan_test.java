0b5a54f17c159577a3417a172caf8eb28a7aeddc
AZR88 <azrielamaldany@gmail.com>
2024-07-31 00:19:59 +0700

    re-configure from junit to TestNG

--- src/test/java/penjumlahan_test.java
+++ src/test/java/penjumlahan_test.java
@@ -1,7 +1,6 @@
 import org.example.Main.penjumlahan;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class penjumlahan_test {
 
@@ -9,28 +8,28 @@ public class penjumlahan_test {
     public void test_penjumlahan_positive_numbers(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(2, 3);
-        assertEquals(5, result, "2 + 3 seharusnya 5");
+        Assert.assertEquals(5, result, "2 + 3 seharusnya 5");
     }
 
     @Test
     public void test_penjumlahan_negative_numbers(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(-2, -3);
-        assertEquals(-5, result, "-2 + -3 seharusnya -5");
+        Assert.assertEquals(-5, result, "-2 + -3 seharusnya -5");
     }
 
     @Test
     public void test_penjumlahan_mixed_numbers(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(-2, 3);
-        assertEquals(1, result, "-2 + 3 seharusnya 1");
+        Assert.assertEquals(1, result, "-2 + 3 seharusnya 1");
     }
 
     @Test
     public void test_penjumlahan_zero(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(0, 0);
-        assertEquals(0, result, "0 + 0 seharusnya 0");
+        Assert.assertEquals(0, result, "0 + 0 seharusnya 0");
     }
 
 
@@ -38,14 +37,14 @@ public class penjumlahan_test {
     public void test_penjumlahan_failure_case(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(2, 3);
-        assertEquals(7, result, "2 + 3 seharusnya bukan 7");
+        Assert.assertEquals(7, result, "2 + 3 seharusnya bukan 7");
     }
 
     @Test
     public void test_penjumlahan_failure_case_pass(){
         penjumlahan calculator = new penjumlahan();
         int result = calculator.add(2, 3);
-        assertNotEquals(7, result, "2 + 3 seharusnya bukan 7");
+        Assert.assertNotEquals(7, result, "2 + 3 seharusnya bukan 7");
     }
 
 
